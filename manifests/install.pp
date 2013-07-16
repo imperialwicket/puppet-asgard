feb5197ff31c53118d736c63f09bf27ab1e68230
Nicholas Whittier <imperialwicket@gmail.com>
2013-07-15 23:39:45 -0700

    Close curlies.

--- manifests/install.pp
+++ manifests/install.pp
@@ -6,7 +6,7 @@ class asgard::install {
 
   exec{
     'rm -rf *':
-      cwd  => "$asgard::tomcat_dir}/webapps",
+      cwd  => "${asgard::tomcat_dir}/webapps",
       path => [ '/bin', '/usr/bin' ];
     'wget asgard':
       command  => "wget https://github.com/Netflix/asgard/releases/download/asgard-${asgard::version}/asgard.war",
